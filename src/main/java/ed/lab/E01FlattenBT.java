4307b067e8f8a523ad7dd8d874fb0c7e5ea67f50
ChristopherLopez <conradoclv@gmail.com>
2025-03-25 07:18:35 -0600

    Parcial 1 Completado

--- src/main/java/ed/lab/E01FlattenBT.java
+++ src/main/java/ed/lab/E01FlattenBT.java
@@ -1,9 +1,27 @@
 package ed.lab;
 
+import java.util.LinkedList;
+
 public class E01FlattenBT {
-
+    LinkedList<TreeNode<Integer>> Result = new LinkedList<>();
     public void flatten(TreeNode<Integer> root) {
+        PreOrder(root);
+        root = CrearArbol(Result);
+    }
 
+    public TreeNode CrearArbol(LinkedList<TreeNode<Integer>> Result) {
+        if(Result.isEmpty()) return null;
+        TreeNode<Integer> root = Result.removeFirst();
+        root.left = null;
+        root.right = CrearArbol(Result);
+        return root;
+    }
+
+    public void PreOrder(TreeNode<Integer> root) {
+        if(root == null) return;
+        Result.add(root);
+        PreOrder(root.left);
+        PreOrder(root.right);
     }
 
 }
